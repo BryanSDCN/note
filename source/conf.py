446e6c2ab05f76eaf0d293547a1da77b7492937d
Bryan <hbcijuly@gmail.com>
2021-02-05 21:05:13 -0500

    Change Project Name

--- source/conf.py
+++ source/conf.py
@@ -46,9 +46,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'note'
-copyright = u'2017, sdwfbobo'
-author = u'sdwfbobo'
+project = u'HappyEveryday'
+copyright = u'2021,Bryan'
+author = u'Bryan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- source/index.rst
+++ source/index.rst
@@ -1,7 +1,7 @@
 .. _home:
 
 ======================
-HaiboCi Note Home
+HAPPY FAMILY
 ======================
 
 .. tip::
